10c551ddf270f408afc43be3d4f56e4a285a6de2
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-05-20 20:53:44 -0700

    mod setup

--- src/main/java/io/github/blobanium/performacraft/PerformacraftManager.java
+++ src/main/java/io/github/blobanium/performacraft/PerformacraftManager.java
@@ -1,14 +1,16 @@
-package net.fabricmc.example;
+package io.github.blobanium.performacraft;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class PerformacraftManager implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		System.out.println("Hello Fabric world!");
+
+		System.out.println("Performacraft Manager");
+		System.out.println("Currently, in this state nothing exists yet.");
 	}
 }
