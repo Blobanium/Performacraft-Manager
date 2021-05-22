18e377677d6d7f81ca7d1645977450c40a3f7ea2
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-05-22 15:43:12 -0700

    Modify TitleChanger

--- src/main/java/io/github/blobanium/performacraft/mixin/MinecraftClientMixin.java
+++ src/main/java/io/github/blobanium/performacraft/mixin/MinecraftClientMixin.java
@@ -1,5 +1,7 @@
 package io.github.blobanium.performacraft.mixin;
 
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.SharedConstants;
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,6 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class MinecraftClientMixin {
     @Inject(at = @At(value = "RETURN"), method = "getWindowTitle", cancellable = true)
     private void getWindowTitle(CallbackInfoReturnable<String> cir){
-        cir.setReturnValue("Performacraft Modpack");
+        cir.setReturnValue("Minecraft " + SharedConstants.getGameVersion().getName() + " / Performacraft " + FabricLoader.getInstance().getModContainer("performacraft").get().getMetadata().getVersion());
     }
 }
