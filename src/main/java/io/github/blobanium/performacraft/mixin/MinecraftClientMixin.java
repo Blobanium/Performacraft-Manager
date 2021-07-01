d28f4b8445195eaf0c169ea487fbbc7b0e8c8dcb
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-07-01 13:11:31 -0700

    Add Various Tweaks To the window title changer

--- src/main/java/io/github/blobanium/performacraft/mixin/MinecraftClientMixin.java
+++ src/main/java/io/github/blobanium/performacraft/mixin/MinecraftClientMixin.java
@@ -4,6 +4,7 @@ import io.github.blobanium.performacraft.PerformacraftManager;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.SharedConstants;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.Session;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,9 +12,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
+    private static int windowTitleCount = 0;
+
     @Inject(at = @At(value = "RETURN"), method = "getWindowTitle", cancellable = true)
     private void getWindowTitle(CallbackInfoReturnable<String> cir){
-        PerformacraftManager.LOGGER.info("Setting Window Title");
-        cir.setReturnValue("Minecraft " + SharedConstants.getGameVersion().getName() + " / Performacraft " + FabricLoader.getInstance().getModContainer("performacraft").get().getMetadata().getVersion());
+        windowTitleCount = windowTitleCount + 1;
+        PerformacraftManager.LOGGER.debug("Setting Window Title (" + windowTitleCount + ")");
+        try {
+            cir.setReturnValue("Minecraft " + SharedConstants.getGameVersion().getName() + " / Performacraft " + FabricLoader.getInstance().getModContainer("performacraft").get().getMetadata().getVersion() + " / " + MinecraftClient.getInstance().getSession().getUsername());
+        } catch (NoClassDefFoundError e) {
+            cir.setReturnValue("Minecraft " + SharedConstants.getGameVersion().getName() + " / Performacraft " + FabricLoader.getInstance().getModContainer("performacraft").get().getMetadata().getVersion());
+        }
     }
 }
